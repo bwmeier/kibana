a11e93d2585cccb2bee284a74a9286a9a87d0320
Boyd Meier <boyd.meier@gmail.com>
2013-08-09 12:46:59 -0500

    Initial durations query panel

---
+++ panels/durations/module.js
@@ -0,0 +1,69 @@
+'use strict';
+
+angular.module('kibana.durations', [])
+.controller('durations', ['$scope', 'querySrv', 'dashboard', 'filterSrv', function($scope, querySrv, dashboard, filterSrv) {
+  $scope.panelMeta = {
+    status  : "Experimental",
+    description : "Experimental durations panel"
+  };
+  
+  // Set and populate defaults
+  var _d = {
+    queries: {
+      mode: 'all',
+      ids: []
+    },
+    status: "Experimental",
+    order: "count",
+    style: {},
+    spyable: true,
+    time_field: "taskname",
+    value_field: "duration",
+  };
+  _.defaults($scope.panel,_d);
+
+  $scope.init = function() {
+    $scope.$on('refresh',function(){
+      $scope.get_data();
+    });
+
+    $scope.get_data();
+  };
+  
+  $scope.get_data = function(segment, query_id) {
+    delete $scope.panel.error;
+
+    // Make sure we have everything for the request to complete
+    if(dashboard.indices.length === 0) {
+      return;
+    }
+    var _range = $scope.range = filterSrv.timeRange('min');
+    
+    if ($scope.panel.auto_int) {
+      $scope.panel.interval = kbn.secondsToHms(
+        kbn.calculate_interval(_range.from,_range.to,$scope.panel.resolution,0)/1000);
+    }
+
+    $scope.panelMeta.loading = true;
+    var _segment = _.isUndefined(segment) ? 0 : segment;
+    var request = $scope.ejs.Request().indices(dashboard.indices[_segment]);
+    var facet = $scope.ejs.TermStatsFacet('stats')
+      .keyField('taskname').valueField('duration')
+      .order($scope.panel.order)
+      .facetFilter(filterSrv.getBoolFilter(filterSrv.ids));
+
+    request = request.facet(facet).size(0);
+    $scope.inspector = angular.toJson(JSON.parse(request.toString()),true);
+
+    request.doSearch().then(function(results){
+      $scope.panelMeta.loading = false;
+      $scope.results = results.facets.stats.terms;
+      $scope.data = _.map($scope.results, function(value) {
+        return {
+          term: value.term,
+          value: value[$scope.panel.order],
+        };
+      });
+    });
+  };
+}]);
