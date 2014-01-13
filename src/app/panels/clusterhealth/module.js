2d4aaa7fde3276d22c0237f9f3bbb9071d9defb8
Boyd Meier <boyd.meier@gmail.com>
2014-01-13 14:03:41 -0600

    refactor clusterhealth panel

--- src/app/panels/clusterhealth/module.js
+++ src/app/panels/clusterhealth/module.js
@@ -1,5 +1,3 @@
-/*jshint globalstrict:true */
-/*global angular:true */
 /*
 
   ## Cluster Status
@@ -9,44 +7,56 @@
 
 */
 
-'use strict';
+define([
+  'angular',
+  'app',
+  'underscore',
+  'jquery',
+  'kbn',
+], function(angular, app, _) {
+  'use strict';
+  
+  var module = angular.module('kibana.panels.health', []);
+  app.useModule(module);
+  
+  module.controller('health', ['$scope', function($scope) {
 
-angular.module('kibana.clusterhealth', [])
-.controller('clusterhealth', ['$scope', function($scope) {
+    $scope.panelMeta = {
+      status  : "Experimental",
+      description : "A status panel for the elasticsearch cluster health"
+    };
 
-  $scope.panelMeta = {
-    status  : "Experimental",
-    description : "A status panel for the elasticsearch cluster health"
-  };
+    var _d = {
+      results: {
+        cluster_name: "Unknown",
+        status: "white",
+      },
+      lastUpdated: "never",
+    };
 
-  var _d = {
-    results: {
-      cluster_name: "Unknown",
-      status: "white",
-    },
-    lastUpdated: "never",
-  };
+    _.defaults($scope, _d);
 
-  _.defaults($scope, _d);
+    $scope.init = function() {
+      $scope.$on('refresh', function() {
+        $scope.get_data();
+      });
 
-  $scope.init = function() {
-    $scope.$on('refresh', function() {
       $scope.get_data();
-    });
+    };
 
-    $scope.get_data();
-  };
+    $scope.get_data = function() {
+      $scope.panelMeta.loading = true;
+      var request = $scope.ejs.client.get('/_cluster/health');
 
-  $scope.get_data = function() {
-    $scope.panelMeta.loading = true;
-    var request = $scope.ejs.client.get('/_cluster/health');
+      request.then(
+        function (results) {
+          $scope.panelMeta.loading = false;
+          $scope.results = results;
+          $scope.lastUpdated = new Date().toString();
+        }
+      );
 
-    request.then(function (results) {
-       $scope.panelMeta.loading = false;
-       $scope.results = results;
-       $scope.lastUpdated = new Date().toString();
-    });
+    };
+  }]);
 
-  };
-}]);
-
+});
